94901f2a7964ce950d272f65e1836f9a95d52560
Frederic Merizen <frederic.merizen@gmail.com>
2014-08-10 21:09:12 +0200

    #171 Take snapshot after scenario

--- src/test/scala/cucumber/selenium/SharedWebDriver.scala
+++ src/test/scala/cucumber/selenium/SharedWebDriver.scala
@@ -2,7 +2,10 @@ package cucumber.selenium
 
 import java.util.concurrent.TimeUnit._
 
+import cucumber.api.Scenario
 import cucumber.util.GlobalCucumberHooks
+import org.openqa.selenium.OutputType._
+import org.openqa.selenium.WebDriverException
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.scalatest.selenium.WebBrowser
 
@@ -20,6 +23,19 @@ trait SharedWebDriver extends WebBrowser with GlobalCucumberHooks {
   Before { _ =>
     webDriver.manage.deleteAllCookies()
   }
+
+  After { scenario: Scenario =>
+    embedScreenShotInReport(scenario)
+  }
+
+  private def embedScreenShotInReport(scenario: Scenario) =
+    try {
+      val screenShot = webDriver.getScreenshotAs(BYTES)
+      scenario.embed(screenShot, "image/png")
+    } catch {
+      case somePlatformsDontSupportScreenShots: WebDriverException =>
+        System.err.println(somePlatformsDontSupportScreenShots.getMessage)
+    }
 
   AfterAll {
     webDriver.quit()
