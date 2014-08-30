fdbed1af5577d0d1f48abbf2e29d57d11a0e4287
Frederic Merizen <frederic.merizen@gmail.com>
2014-08-30 13:37:53 +0200

    #170 Add validation messages on signup form

--- src/main/scala/org/merizen/protouser/ProtoUser.scala
+++ src/main/scala/org/merizen/protouser/ProtoUser.scala
@@ -3,25 +3,31 @@ package org.merizen.protouser
 import net.liftweb.common.Full
 import net.liftweb.http.{S, SHtml}
 import net.liftweb.proto.{ProtoUser => UnderlyingProtoUser}
+import net.liftweb.util.BaseField
 import net.liftweb.util.Helpers._
 
 import scala.xml.NodeSeq
 
 trait ProtoUser extends UnderlyingProtoUser {
-  protected override def localForm(user: TheUserType, ignorePassword: Boolean, fields: List[FieldPointerType]): NodeSeq = {
+
+  protected override def localForm(user: TheUserType, ignorePassword: Boolean, fields: List[FieldPointerType]): NodeSeq =
     for {
       pointer <- fields
       field <- computeFieldFromPointer(user, pointer).toList
       if field.show_? && (!ignorePassword || !pointer.isPasswordField_?)
       form <- field.toForm.toList
       finalField <- if(pointer.isPasswordField_?) {
-        <tr><td>{ S.?("password") }</td><td><user:pwd/></td></tr>
+        <tr><td>{ S.?("password") }</td><td><user:pwd/>{messageForField(field)}</td></tr>
         <tr><td>{ S.?("repeat") }</td><td><user:pwd/></td></tr>
       } else {
-        <tr><td>{field.displayName}</td><td>{form}</td></tr>
+        <tr><td>{field.displayName}</td><td>{form}{messageForField(field)}</td></tr>
       }
     } yield finalField
-  }
+
+  private def messageForField(field: BaseField): NodeSeq = (
+    for {
+      idField <- field.uniqueFieldId
+    } yield <lift:Msg id={idField}>message</lift:Msg>).getOrElse(Nil)
 
   override def signup = {
     val theUser: TheUserType = mutateUserOnSignup(createNewUserInstance())
