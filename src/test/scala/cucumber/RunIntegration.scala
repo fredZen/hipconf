73d38a87f2947ff98e8de3292fd0c1f9948683ed
Frederic Merizen <frederic.merizen@gmail.com>
2015-01-01 22:33:42 +0100

    Enable cucumber pro publisher

--- src/test/scala/cucumber/RunIntegration.scala
+++ src/test/scala/cucumber/RunIntegration.scala
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith
 @CucumberOptions(
   features = Array("classpath:cucumber"),
   glue = Array("cucumber.integration"),
-  plugin = Array("html:target/cucumber"),
+  plugin = Array("html:target/cucumber", "cucumber.pro.CucumberProFormatter"),
   tags = Array("@integration")
 )
 class RunIntegration
